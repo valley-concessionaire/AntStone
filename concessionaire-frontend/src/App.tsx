f1d1624126015a102b1399503d3827dda771956b
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2024-03-30 16:31:23 -0500

    Primera vista del login con el captcha manual

--- concessionaire-frontend/src/App.tsx
+++ concessionaire-frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './valley.jpg';
+
 import './App.css';
+import { ContenedorLogin } from './componentes/ContainerLogin';
 
 function App() {
   return (
     <div className="App">
+      {/* 
       <header className="App-header">
         <img src={logo} className="App-logo rounded-full m-16" alt="logo" />
         <p>
@@ -19,6 +22,14 @@ function App() {
           Here begins this stormy history
         </a>
       </header>
+      */}
+      <header className="header1">
+        <h1>Empresa de obras</h1>
+      </header>
+      <ContenedorLogin />
+      <footer className='footer1'>
+        Aqui se pone algo supongo
+      </footer>
     </div>
   );
 }
