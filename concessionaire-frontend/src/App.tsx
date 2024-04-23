02e09f580fb1ff5a90f80471b5e9b7e07baae951
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-04-23 01:19:35 -0500

    Se organizó un poco el front-end y se incluyó shadcn-ui. Se actualizó la página de inicio de sesión con base en lo mostrado en la primer sustentación

--- concessionaire-frontend/src/App.tsx
+++ concessionaire-frontend/src/App.tsx
@@ -1,35 +1,12 @@
 import React from 'react';
 import logo from './valley.jpg';
 
-import './App.css';
-import { ContenedorLogin } from './componentes/ContainerLogin';
+import { LoginPage } from './pages/loginPage';
 
 function App() {
   return (
     <div className="App">
-      {/* 
-      <header className="App-header">
-        <img src={logo} className="App-logo rounded-full m-16" alt="logo" />
-        <p>
-          I am the Scrum Master, don't I?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Here begins this stormy history
-        </a>
-      </header>
-      */}
-      <header className="header1">
-        <h1>Empresa de obras</h1>
-      </header>
-      <ContenedorLogin />
-      <footer className='footer1'>
-        Aqui se pone algo supongo
-      </footer>
+      <LoginPage/>
     </div>
   );
 }
