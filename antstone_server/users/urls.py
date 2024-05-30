c767ec3585d14bfd0f93d01c8d9dd544b68cdca0
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-05-30 15:06:42 -0700

    Adding custom token generation for user data retrieve

--- antstone_server/users/urls.py
+++ antstone_server/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from users.views import CustomTokenObtainPairView
 from .viewsets import (
     CapatazViewSet, DirectorDeObraViewSet, GerenteViewSet,
     PeonViewSet, AyudanteDeAlbanilViewSet
 )
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'capataz', CapatazViewSet, basename='capataz')
@@ -15,6 +16,6 @@ router.register(r'ayudante-de-albanil', AyudanteDeAlbanilViewSet, basename='ayud
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('authentication', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
