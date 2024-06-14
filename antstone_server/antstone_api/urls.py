e08726f6ca1959d15ebd987f138b7692c94529fa
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2024-06-14 01:06:15 -0500

    Viewsets y registro de url realizado para imagenAvances.

--- antstone_server/antstone_api/urls.py
+++ antstone_server/antstone_api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.documentation import include_docs_urls
-from .viewsets import ObraViewSet, TareaDeObraViewSet, AvanceDeTareaViewSet
+from .viewsets import ObraViewSet, TareaDeObraViewSet, AvanceDeTareaViewSet, ImagenAvanceViewSet
 from antstone_api import api_views
 
 router = DefaultRouter()
 router.register(r'obras', ObraViewSet, basename='obra')
 router.register(r'tareas', TareaDeObraViewSet, basename='tarea')
 router.register(r'avances', AvanceDeTareaViewSet, basename='avance')
+router.register(r'imagenavances', ImagenAvanceViewSet, basename='imagen_avance')
 
 urlpatterns = [
     path('', include(router.urls)),
