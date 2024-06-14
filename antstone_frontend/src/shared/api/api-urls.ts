32eadc9c0853d9bc1b776f08ed5f0491f144a52e
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-06-13 20:04:20 -0700

    Fix a error in urls file

--- antstone_frontend/src/shared/api/api-urls.ts
+++ antstone_frontend/src/shared/api/api-urls.ts
@@ -2,7 +2,7 @@ export const AuthenticateEndpoint = "/token";
 export const RefreshTokenEndpoint = "/refresh-token";
 export const GetAyudanteDeAbanilEndpoint = "/users/ayudante-de-albanil/";
 export const GetACapatazEndpoint = "/users/capataz/";
-export const GetObrasEndpoint =  "/obras/";
+export const GetObrasEndpoint =  () => "/obras/";
 export const PostObrasEndpoint = () =>  "/obras/";
 export const GetAvancesEndpoint =  "/avances/";
 export const PostAvancesEndpoint = () => "/avances/";
